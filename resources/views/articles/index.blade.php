363eac8a2744ed3bc684834ab9fa31444a2f5a00
tahanialhammad <tahani_alhammad@yahoo.com>
2021-01-22 15:48:51 +0100

    LaravelBlog Consider Named Routes, index, show

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -7,7 +7,7 @@
     @foreach ($articles as $article)
     <div>
         <div class="title">
-            <h3><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></h3>
+        <h3><a href="{{ $article->path() }}">{{ $article->title }}</a></h3>
         </div>
         <img src="/images/banner.jpg" alt="" class="img-fluid" />
         <p>{{ $article->excerpt }}</p>
