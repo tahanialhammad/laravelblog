5fac79e27c9988c735d183e5e5f35f0d6afe1ccf
tahanialhammad <tahani_alhammad@yahoo.com>
2021-01-30 13:31:05 +0100

    Add Sass

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,7 +5,7 @@
 <div class="container">
     <div class="title">
         <h2>{{ $article->title }}</h2>
-        <span class="text-muted">Mauris vulputate dolor sit amet nibh</span> </div>
+        <span class="text-muted">Posted by {{ $article->user->name }} </span> </div>
     </div>
     <img src="/images/banner.jpg" alt="" class="img-fluid" />
     <p>{{ $article->body }}</p>
