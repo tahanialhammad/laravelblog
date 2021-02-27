89c5ed4830c3c51536c50882d3b405820377e6d4
tahanialhammad <tahani_alhammad@yahoo.com>
2021-02-27 21:58:06 +0100

    update layout

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,69 @@
+
+<nav class="container">
+    <div class="row">
+        <div class="col-sm">
+        <i class="fa fa-phone"></i>0625487
+        <i class="far fa-envelope"></i>tahani@tahani.com
+        </div>
+        <div class="col-sm">
+        adreess groningen
+        </div>
+    </div>
+</nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
+    <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link {{ Request::path() === '/home' ? 'active' : ''}}" aria-current="page" href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::path() === '/about' ? 'active' : ''}}" href="/about">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::path() === '/articles' ? 'active' : ''}}" href="/articles">Articles</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::path() === '/articles/create' ? 'active' : ''}}" href="/articles/create">New Articles</a>
+        </li>
+      </ul>
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav">
+        @guest
+            @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+            @endif
+
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+            @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ Auth::user()->name }}
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                    </a></li>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </ul>
+             </li>
+        @endguest
+      </ul>
+    </div>
+  </div>
+</nav>
