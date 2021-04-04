7b32fce452d12cb4ffb8590dc687ad0502fd4aec
tahanialhammad <tahani_alhammad@yahoo.com>
2021-04-04 15:37:44 +0200

    update article style

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -12,7 +12,7 @@
             <p>Ik ben Tahani Alhammad woon in Groningen en kom oorspronkelijk uit Irak, waar ik computer engineer was. Bestond mijn werk daar voornamelijk uit onderzoek naar o.a. netwerkapparaten, inmiddels weet ik dat ik de kant van front end developer op wil. Hoewel mijn passie daar ligt, heb ik ook interesse in de back end-kant van programmeren. Ik vond CodeGorilla-avontuur als een van de tofste dingen die ik ooit heeft gedaan. Mijn programmertalen zijn : HTML5/CSS3, SASS, Bootstrap, JS, NodeJS, Express, EJS, VueJs, JSON, Mongoose, MongoDB, PHP, MySQL en Laravel</p>
         </div>
         <div class="col-md-4">
-            <div class="container">
+            <div class="container article">
                 <ul class="list-group">
                     @foreach ($articles as $article)
                     <li class="list-group-item">
