3090f7961ac41269ac27596382340d6c2bdb1b35
tahanialhammad <tahani_alhammad@yahoo.com>
2021-04-05 19:26:34 +0200

    update style

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -9,19 +9,19 @@
 <!-- Cross-site request forgery -->
 @csrf
   <div class="mb-3">
-    <label for="titleInput" class="form-label">Article Title</label>
+    <label for="titleInput" class="form-label">Artikel titel</label>
     <input type="text" class="form-control" id="titleInput" name="title" required>
   </div>
   <div class="mb-3">
-    <label for="imageurl" class="form-label">Article Image</label>
+    <label for="imageurl" class="form-label">Artikel afbeelding</label>
     <input type="text" class="form-control" id="imageurl" name="image" required>
   </div>
   <div class="mb-3">
-    <label for="excerpTextarea" class="form-label">Article excerpt</label>
+    <label for="excerpTextarea" class="form-label">Artikel beschrijven</label>
     <textarea class="form-control" id="excerpTextarea" name="excerpt" rows="3" required></textarea>
   </div>
   <div class="mb-3">
-    <label for="bodyTextarea" class="form-label">Article</label>
+    <label for="bodyTextarea" class="form-label">Artikel</label>
     <textarea class="form-control" id="bodyTextarea" name="body" rows="3" required></textarea>
   </div>
 
@@ -32,7 +32,7 @@
         @endforeach
     </select>
   </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Verzenden</button>
   </form>
 
 </div>
--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -11,19 +11,19 @@
 @method('PUT')
 
   <div class="mb-3">
-    <label for="titleInput" class="form-label">Article Title</label>
+    <label for="titleInput" class="form-label">Artikel titel</label>
     <input type="text" class="form-control" id="titleInput" name="title" value="{{ $article->title }}" required>
   </div>
   <div class="mb-3">
-    <label for="imageurl" class="form-label">Article Image</label>
+    <label for="imageurl" class="form-label">Artikel afbeelding</label>
     <input type="text" class="form-control" id="imageurl" name="image"  value="{{ $article->image }}" required>
   </div>
   <div class="mb-3">
-    <label for="excerpTextarea" class="form-label">Article excerpt</label>
+    <label for="excerpTextarea" class="form-label">Artikel beschrijven</label>
     <textarea class="form-control" id="excerpTextarea" name="excerpt" rows="3" required>{{ $article->excerpt }}</textarea>
   </div>
   <div class="mb-3">
-    <label for="bodyTextarea" class="form-label">Article</label>
+    <label for="bodyTextarea" class="form-label">Artikel</label>
     <textarea class="form-control" id="bodyTextarea" name="body" rows="3" required>{{ $article->body }}</textarea>
   </div>
 
@@ -37,7 +37,7 @@
 
 
 
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Verzenden</button>
   </form>
 
 </div>
